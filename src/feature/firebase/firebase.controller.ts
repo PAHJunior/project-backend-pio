2fc945cf097cb4a5eafb3ef5a501f058f2e060fd
Paulo Junior <paulo.junior@gorilainvest.com.br>
2021-12-02 23:49:30 -0300

    add tag firebase for swagger

--- src/feature/firebase/firebase.controller.ts
+++ src/feature/firebase/firebase.controller.ts
@@ -7,7 +7,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { FirebaseAuthDto } from './dto/firebase-auth.dto';
 import { FirebaseService } from './firebase.service';
 
-@ApiTags('send-mail')
+@ApiTags('firebase')
 @Controller('firebase')
 export class FirebaseController {
   constructor(private readonly firebaseService: FirebaseService) {}
