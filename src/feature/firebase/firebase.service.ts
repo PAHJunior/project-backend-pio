e32ae0f455002dd0c7fd2705f00c464725030398
Paulo Junior <paulo.junior@gorilainvest.com.br>
2021-12-02 23:44:58 -0300

    add config firebase in .env

--- src/feature/firebase/firebase.service.ts
+++ src/feature/firebase/firebase.service.ts
@@ -54,12 +54,12 @@ export class FirebaseService {
    */
   private initializeFirebase(): FirebaseApp {
     const firebaseConfig = {
-      apiKey: 'AIzaSyC-xYQFUQj4ovlXEsxVtsyjnGhhf7QZYHk',
-      authDomain: 'projectpio.firebaseapp.com',
-      projectId: 'projectpio',
-      storageBucket: 'projectpio.appspot.com',
-      messagingSenderId: '1015721177438',
-      appId: '1:1015721177438:web:891068741fdf5eda05dfcf',
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
     };
 
     return initializeApp(firebaseConfig);
